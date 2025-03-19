90387b19e00e3e3dddb621abe3819b410b868eed
Umar Rajput <47900850+umarraj008@users.noreply.github.com>
2025-03-19 19:37:28 +0000

    front end and backend init

---
+++ anime-tracker-app/frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}", 
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}" // Include if using Next.js 13+ with App Router
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
